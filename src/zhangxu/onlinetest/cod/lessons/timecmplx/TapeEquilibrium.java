5d47ba84358a96917c05bde6dbbf27a890d4e863
AntiiiMage <zhangxu.otis@gmail.com>
2016-04-04 20:56:53 +0800

    Fix the bug

--- src/zhangxu/onlinetest/cod/lessons/timecmplx/TapeEquilibrium.java
+++ src/zhangxu/onlinetest/cod/lessons/timecmplx/TapeEquilibrium.java
@@ -3,20 +3,20 @@ package zhangxu.onlinetest.cod.lessons.timecmplx;
 public class TapeEquilibrium {
 
 	
-	public static int solution(int[] A, int N){
+	public int solution(int[] A, int N){
 		int min = Integer.MAX_VALUE;
-		for(int i = 0; i < N; i ++){
+		for(int i = 0; i < N - 1; i ++){
 			int diff = getAbsoluteDiff(sumArr(A, 0, i), sumArr(A, i + 1, N - 1));
 			min = min > diff ? diff : min;
 		}
 		return min;
 	}
 	
-	private static int getAbsoluteDiff(int a, int b){
+	private int getAbsoluteDiff(int a, int b){
 		return a > b ? a - b : b - a;
 	}
 	
-	private static int sumArr(int[] A, int s, int e){
+	private int sumArr(int[] A, int s, int e){
 		int result = 0;
 		for(int i = s; i <= e; i ++){
 			result += A[i];
--- src/zhangxu/onlinetest/cod/test/Runner.java
+++ src/zhangxu/onlinetest/cod/test/Runner.java
@@ -9,9 +9,10 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		CyclicRotation cr = new CyclicRotation();
-		int[] A = { 3, 1, 2, 4 ,3};
+		TapeEquilibrium tb = new TapeEquilibrium();
+		int[] A = { -1, 1};
 		int N = A.length;
-		System.out.println(TapeEquilibrium.solution(A, N));
+		System.out.println(tb.solution(A, N));
 //		int k = 2;
 //		Printer.printIntArrInLine(A);
 //		Printer.printIntArrInLine(cr.reversalRotate(A, N, k));
